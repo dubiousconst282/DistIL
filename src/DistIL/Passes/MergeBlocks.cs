9673b5e08d7de72740281d9e96394fb19478cf5d
dubiousconst282 <87553666+dubiousconst282@users.noreply.github.com>
2022-04-17 04:23:46 -0300

    Fix transform that removes blocks with single goto

--- src/DistIL/Passes/MergeBlocks.cs
+++ src/DistIL/Passes/MergeBlocks.cs
@@ -70,6 +70,14 @@ public class MergeBlocks : Pass
 
         var pred = b1.Preds[0];
         var succ = b1.Succs[0];
+        //Ensure that removing this block won't duplicate edges/make a ambiguous phi:
+        //  goto cond ? B1 : B2
+        //  B1: goto B3
+        //  B2: goto B3
+        //  B3: val = phi [B1 -> x, B2 -> y]
+        //We can remove either B1 or B2, but not both.
+        //TODO: we can still remove it (and dedup edges) if there's no phi in succ.
+        if (succ.Preds.Contains(pred)) return false;
 
         pred.Succs.Remove(b1);
         pred.Succs.Add(succ);
@@ -77,7 +85,11 @@ public class MergeBlocks : Pass
         succ.Preds.Remove(b1);
         succ.Preds.Add(pred);
 
-        b1.ReplaceUses(succ);
+        foreach (var (inst, operIdx) in b1.Uses.ToArray()) {
+            Assert(inst.Block == pred || inst.Block == succ);
+            var replBlock = inst is PhiInst ? pred : succ;
+            inst.ReplaceOperand(operIdx, replBlock);
+        }
         b1.Remove();
         return true;
     }
